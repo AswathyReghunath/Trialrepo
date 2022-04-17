b2e4af1cad011e6b2ffddce31d4d13f47108751d
Nimmiya <nimmiya@designdirect.io>
2022-04-17 16:28:26 +0530

    first commit

---
+++ src/Login.js
@@ -0,0 +1,11 @@
+import react from 'react'
+import { View, Text } from 'react-native'
+
+function Apple(){
+    return(
+        <View>
+            <Text>This is an apple</Text>
+        </View>
+    )
+}
+export default Apple()
